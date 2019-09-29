9f3eb0223fdbdf2519f332d6a4bf894c73be1d8b
Masudur Rahman <munnaonc@gmail.com>
2019-09-29 19:33:54 +1000

    Added comment for the object state

--- jobsite/src/src/main/java/com/neet/jobsite/model/ObjectState.java
+++ jobsite/src/src/main/java/com/neet/jobsite/model/ObjectState.java
@@ -1,7 +1,17 @@
 package com.neet.jobsite.model;
 
 public enum ObjectState {
+	/*Unchanged State
+	 * */
 	Unchanged,
+	
+	/* Modified state could be for unsaved changes
+	 * For object needed to be insert for the first time would have ID as null
+	 * For object already added to database shall have ID as value.
+	 * */
 	Modified,
+	
+	/* When the object is deleted this state would be used to determine that object is deleted
+	 * */
 	Deleted
 }
