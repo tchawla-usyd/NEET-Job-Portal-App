eb4636d375b5eb84d0bfd464c4fcabe80ea4f301
Masudur Rahman <munnaonc@gmail.com>
2019-09-29 22:49:38 +1000

    Added Repository template.

--- jobsite/src/src/main/java/com/neet/jobsite/dal/AdminUserRepository.java
+++ jobsite/src/src/main/java/com/neet/jobsite/dal/AdminUserRepository.java
@@ -1,5 +1,33 @@
 package com.neet.jobsite.dal;
 
-public class AdminUserRepository {
+import com.neet.jobsite.model.User;
+import java.util.ArrayList;
 
+/*This Administrator Repository to maintain all the users*/
+public class AdminUserRepository extends BaseRepository{
+	
+	/* Add a new user */
+	public User Add(User user) {
+		return null;
+	}
+	
+	/* Update the user */
+	public User Update(User user) {
+		return null;
+	}
+	
+	/* Get user by id */
+	public User GetById(Integer UID) {
+		return null;
+	}
+	
+	/* Get all the users */
+	public  ArrayList<User> GetAll() {
+		return null;
+	}
+
+	/* Delete an user from the repository */
+	public User Delete(User user) {
+		return null;
+	}
 }
