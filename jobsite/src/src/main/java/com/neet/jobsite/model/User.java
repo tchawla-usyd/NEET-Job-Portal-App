ebb0034ae00e9eb6c5fa918b60faad31f62c21b3
Masudur Rahman <munnaonc@gmail.com>
2019-09-29 20:01:35 +1000

    Added comment for the base object

--- jobsite/src/src/main/java/com/neet/jobsite/model/User.java
+++ jobsite/src/src/main/java/com/neet/jobsite/model/User.java
@@ -1,4 +1,18 @@
 package com.neet.jobsite.model;
 
 public class User extends BaseObject { 
+	/*
+	  `UID` INT(11) NOT NULL,
+	  `FirstName` VARCHAR(45) NULL DEFAULT NULL,
+	  `LastName` VARCHAR(45) NULL DEFAULT NULL,
+	  `Email` VARCHAR(45) NULL DEFAULT NULL,
+	  `Password` VARCHAR(45) NULL DEFAULT NULL,
+	  `IsActive` TINYINT(4) NULL DEFAULT NULL,
+	  `IsLocked` TINYINT(4) NULL DEFAULT NULL,
+	  `CreatedDate` DATETIME NOT NULL,
+	  `ModifiedDate` DATETIME NOT NULL,
+	  `UserTypeID` INT(11) NULL DEFAULT NULL,
+	  */
+	
+	
 }
