cd1c95b03788a3efd1f8c5fa229aab411024f56f
Masudur Rahman <munnaonc@gmail.com>
2019-10-05 18:49:44 +1000

    Admin Dashboard

--- jobsite/src/src/main/java/com/neet/jobsite/admin/AdminDashboardController.java
+++ jobsite/src/src/main/java/com/neet/jobsite/admin/AdminDashboardController.java
@@ -1,5 +1,54 @@
 package com.neet.jobsite.admin;
 
-public class AdminDashboardController {
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.neet.jobsite.HomeController;
+
+@Controller
+@RequestMapping(value="/admin/**")
+public class AdminDashboardController {
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "admin/home";
+	}
+	
+	
+	@RequestMapping(value = "/skillsets", method = RequestMethod.GET)
+	public String skillsets(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "admin/skillsetlists";
+	}
+	
+	
 }
