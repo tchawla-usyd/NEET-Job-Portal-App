8264c616ae2e6f49b817b22aec0c5b7bc4c979bb
Gavin Fungtammasan <gavin.kiwi@gmail.com>
2019-10-20 18:26:11 +1100

    everything

--- jobsite/src/src/main/java/com/neet/jobsite/bal/CandidateService.java
+++ jobsite/src/src/main/java/com/neet/jobsite/bal/CandidateService.java
@@ -1,6 +1,10 @@
 package com.neet.jobsite.bal;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +14,9 @@ import com.neet.jobsite.dal.CandidateManager;
 import com.neet.jobsite.dal.JobManager;
 import com.neet.jobsite.model.Candidate;
 import com.neet.jobsite.model.CandidateJobApplied;
+import com.neet.jobsite.model.SkillSet;
+import com.neet.jobsite.model.User;
+import com.neet.jobsite.response.ApplicantsResponse;
 
 @Service(value = "candidateService")
 public class CandidateService {
@@ -17,6 +24,7 @@ public class CandidateService {
 	@Autowired
 	@Qualifier("candidateManager")
 	private CandidateManager candidateManager;
+	
 	
 	public void applyJob(Integer jobId, String userToken) {
 		if (isValidUser(userToken)) {
@@ -42,6 +50,51 @@ public class CandidateService {
 		
 	}
 	
+	public List<SkillSet> getCandidateSkills(Long candidateId, String userToken) {
+		List<SkillSet> skills = candidateManager.getCandidateSkillsById(candidateId);
+		return skills;
+	}
+	
+	public List<ApplicantsResponse> getApplicants(Long jobId, String userToken) {
+		if (isValidUser(userToken)) {
+			Integer userId = getUserId(userToken);
+			
+			List<CandidateJobApplied> applications = candidateManager.getApplicationInfo(jobId);
+			
+			List<User> users = candidateManager.getApplicants(jobId);
+			
+			List<Long> ids = new ArrayList<Long>();
+			
+			for(User user: users) {
+				ids.add(user.getId());
+			}
+						
+			List<ApplicantsResponse> applicants = new ArrayList<ApplicantsResponse>();
+			Format formatter = new SimpleDateFormat("yyyy-MM-dd");
+			
+			for(int i = 0; i < ids.size(); i++) {
+				ApplicantsResponse app = new ApplicantsResponse();
+				app.setUser_id(users.get(i).getId());
+				app.setFirst_name(users.get(i).getFirstName());
+				app.setLast_name(users.get(i).getLastName());
+				app.setEmail(users.get(i).getEmail());
+				app.setApplyDate(applications.get(i).getApplyDate().toString());
+				
+				List<SkillSet> skills = candidateManager.getCandidateSkillsById(users.get(i).getId());
+				app.setSkills(skills);
+				
+				applicants.add(app);
+			}
+			
+			return applicants;
+		}
+		
+		return null;
+		
+	}
+	
+	
+	
 	
 	private Integer getUserId(String userToken) {
 		// TODO Auto-generated method stub
@@ -52,6 +105,11 @@ public class CandidateService {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
+
+
+	
+	
 
 
 
--- jobsite/src/src/main/java/com/neet/jobsite/dal/CandidateManager.java
+++ jobsite/src/src/main/java/com/neet/jobsite/dal/CandidateManager.java
@@ -1,9 +1,13 @@
 package com.neet.jobsite.dal;
 
 import java.io.Serializable;
+import java.util.List;
 
 import com.neet.jobsite.model.Candidate;
 import com.neet.jobsite.model.CandidateJobApplied;
+import com.neet.jobsite.model.CandidateSkills;
+import com.neet.jobsite.model.SkillSet;
+import com.neet.jobsite.model.User;
 
 public interface CandidateManager extends Serializable {
 
@@ -14,6 +18,14 @@ public interface CandidateManager extends Serializable {
 	void editCandidateInfo(Candidate candidate);
 
 	Candidate getCandidateById(Integer userId);
+
+	List<User> getApplicants(Long jobId);
+
+	List<Candidate> getCandidateByIds(List<Long> ids);
+	
+	List<SkillSet> getCandidateSkillsById(Long ids);
+
+	List<CandidateJobApplied> getApplicationInfo(Long jobId);
 
 	
 	
