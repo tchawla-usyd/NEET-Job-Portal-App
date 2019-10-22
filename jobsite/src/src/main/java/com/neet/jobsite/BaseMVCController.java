0083502a30c5e69878bd3dda8474af1130da8424
Gavin Fungtammasan <gavin.kiwi@gmail.com>
2019-10-22 22:40:57 +1100

    added authorization header to authorized API and dummy auhorization by token

--- jobsite/src/src/main/java/com/neet/jobsite/BaseMVCController.java
+++ jobsite/src/src/main/java/com/neet/jobsite/BaseMVCController.java
@@ -1,14 +1,20 @@
 package com.neet.jobsite;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.neet.jobsite.bal.IAuthenticateService;
+
 public class BaseMVCController {
 
 	@Autowired
 	protected HttpServletRequest context;
+	
+	@Resource(name = "authenticateBal")
+	private IAuthenticateService authenticateBal;
 
 	public boolean IsAuthenticated() {
 		HttpSession session = context.getSession(false);
@@ -19,5 +25,19 @@ public class BaseMVCController {
 			// user IS logged in, do something: set model or do whatever you need
 			return true;
 		}
+	}
+	
+	public boolean authenticateByToken(String token) {
+		if(token == null)
+			return false;
+		
+		HttpSession session = context.getSession(true);
+		if(token.equals("testBadToken")){
+			return false;
+		}
+		
+		session.setAttribute("userId", 1);
+		return true;
+		
 	}
 }
--- jobsite/src/src/main/java/com/neet/jobsite/JobController.java
+++ jobsite/src/src/main/java/com/neet/jobsite/JobController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neet.jobsite.bal.JobService;
+import com.neet.jobsite.response.ErrorResponse;
 import com.neet.jobsite.response.JobResponse;
 
 @Controller
@@ -34,7 +38,7 @@ public class JobController  extends BaseMVCController {
 
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void addJob(HttpServletRequest httpServletRequest) {
+	public void addJob(HttpServletRequest httpServletRequest, HttpServletResponse response, @RequestHeader("Authorization") String userToken) {
 		
 		String title = httpServletRequest.getParameter("title");
 		String description = httpServletRequest.getParameter("description");
@@ -42,22 +46,24 @@ public class JobController  extends BaseMVCController {
 		String startDate = httpServletRequest.getParameter("start_date");
 		String endDate = httpServletRequest.getParameter("end_date");
 		Integer jobCategory = Integer.parseInt(httpServletRequest.getParameter("job_category"));
-		System.out.println(httpServletRequest.getParameterValues("skills"));
 		List<String> skills = Arrays.asList(httpServletRequest.getParameterValues("skills"));
-		System.out.println(skills);
-
-		// authorized token to get user id
-		String userToken = httpServletRequest.getParameter("token");
 		
 		logger.info(title);
-			
-		jobService.addJob(title, description, location, startDate, endDate, jobCategory, userToken, skills);
-
+		
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			jobService.addJob(title, description, location, startDate, endDate, jobCategory, userId, skills);
+		}
+		else {
+			response.setStatus(403);
+		}
 	}
 	
 	@RequestMapping(value="/edit", method=RequestMethod.POST)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void editJob(HttpServletRequest httpServletRequest) {
+	public void editJob(HttpServletRequest httpServletRequest, HttpServletResponse response,
+			@RequestHeader("Authorization") String userToken) {
 		
 		long UID = Long.parseLong(httpServletRequest.getParameter("job_id"));
 		String title = httpServletRequest.getParameter("title");
@@ -66,48 +72,68 @@ public class JobController  extends BaseMVCController {
 		String startDate = httpServletRequest.getParameter("start_date");
 		String endDate = httpServletRequest.getParameter("end_date");
 		String jobCategory_tmp = httpServletRequest.getParameter("job_category");
-		Integer jobCategory = jobCategory_tmp == null ? null : Integer.parseInt(jobCategory_tmp);
-		
-		// authorized token to get user id
-		String userToken = httpServletRequest.getParameter("token");
+		Integer jobCategory = jobCategory_tmp == null ? null : Integer.parseInt(jobCategory_tmp);		
 		
 		List<String> skills = null;
 		if (httpServletRequest.getParameterValues("skills") != null)
 				skills = Arrays.asList(httpServletRequest.getParameterValues("skills"));
 		
-		logger.info(title);
-			
-		jobService.editJob(UID, title, description, location, startDate, endDate, jobCategory, userToken, skills);
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			jobService.editJob(UID, title, description, location, startDate, endDate, jobCategory, userId, skills);
+		}
+		else {
+			response.setStatus(403);
+		}
 
 	}
 	
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void deleteJob(@PathVariable("id") Integer id) {
+	public void deleteJob(@PathVariable("id") Integer id, HttpServletResponse response,
+			@RequestHeader("Authorization") String userToken) {
 		
-		// authorized token
-		String authToken = "abcd";
-		
-		jobService.deleteJob(id, authToken);
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			jobService.deleteJob(id, userId);
+		}
+		else {
+			response.setStatus(403);
+		}
 	}
 
 	@RequestMapping(value="/getjob/{id}", 
 			method=RequestMethod.GET, 
 			produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String getJob(@PathVariable("id") Long id) {
+	public String getJob(@PathVariable("id") Long id,  HttpServletResponse response,
+			@RequestHeader("Authorization") String userToken) {
 		
-		JobResponse job = jobService.getJob(id);
 		
 		ObjectMapper objectMapper = new ObjectMapper();
-		
 		String jsonReturn = null;
-		try {
-			jsonReturn = objectMapper.writeValueAsString(job);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
 		
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			JobResponse job = jobService.getJob(id);
+			try {
+				jsonReturn = objectMapper.writeValueAsString(job);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			response.setStatus(403);
+			try {
+				jsonReturn = objectMapper.writeValueAsString(new ErrorResponse("Authentication Failed"));
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+			
 		return jsonReturn;
 	}
 	
@@ -115,37 +141,64 @@ public class JobController  extends BaseMVCController {
 			method=RequestMethod.GET, 
 			produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String getJobByEmployer(@PathVariable("id") Long employer_id, Model uiModel) {
-		List<JobResponse> jobs = jobService.getJobByEmployer(employer_id);
+	public String getJobByEmployer(@PathVariable("id") Long employer_id, 
+			HttpServletResponse response, @RequestHeader("Authorization") String userToken) {
+		
 		
 		ObjectMapper objectMapper = new ObjectMapper();
-		
 		String jsonReturn = null;
-		try {
-			jsonReturn = objectMapper.writeValueAsString(jobs);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+	
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			List<JobResponse> jobs = jobService.getJobByEmployer(employer_id);
+			try {
+				jsonReturn = objectMapper.writeValueAsString(jobs);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
 		}
-		
+		else {
+			response.setStatus(403);
+			try {
+				jsonReturn = objectMapper.writeValueAsString(new ErrorResponse("Authentication Failed"));
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return jsonReturn;
 	}
 
 	@RequestMapping(value="/getjob/{job_id}/add/skill/{skill_id}", method=RequestMethod.PUT)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void addSkillToJob(@PathVariable("job_id") Integer jobId, @PathVariable("skill_id") Integer skillId) {
-		
-		String authToken = "abcd";
-		
-		jobService.addSkillToJob(jobId, skillId, authToken);
+	public void addSkillToJob(@PathVariable("job_id") Integer jobId, @PathVariable("skill_id") Integer skillId, 
+			HttpServletResponse response, @RequestHeader("Authorization") String userToken) {
+				
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			jobService.addSkillToJob(jobId, skillId, userId);
+		}
+		else {
+			response.setStatus(403);
+		}
 	}
 	
 	@RequestMapping(value="/getjob/{job_id}/delete/skill/{skill_id}", method=RequestMethod.DELETE)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void deleteSkillFromJob(@PathVariable("job_id") Integer jobId, @PathVariable("skill_id") Integer skillId) {
+	public void deleteSkillFromJob(@PathVariable("job_id") Integer jobId, @PathVariable("skill_id") Integer skillId,
+			HttpServletResponse response, @RequestHeader("Authorization") String userToken) {
+			
+		if(authenticateByToken(userToken)) {
+			HttpSession session = context.getSession(false);
+			Integer userId = (Integer) session.getAttribute("userId");
+			jobService.deleteSkillFromJob(jobId, skillId, userId);		
+		}
+		else {
+			response.setStatus(403);
+		}
 		
-		String authToken = "abcd";
-		
-		jobService.deleteSkillFromJob(jobId, skillId, authToken);		
 	}
 	
 	
