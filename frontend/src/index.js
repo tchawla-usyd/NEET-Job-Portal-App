3c845519d5780c7f4724f6ae02acd6487553aae0
Rex Shen <Admin@vlan-2666-10-17-16-26.staff.wireless.sydney.edu.au>
2019-10-26 16:01:23 +1100

    remove redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./js/components/App";
-
-import store from "./js/store/index";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Provider store={store}>
+ReactDOM.render(
      <Router>
      	<App />
-     </Router>
-  </Provider>, 
+     </Router>, 
   document.getElementById('root'));
 
--- frontend/src/js/store/index.js
+++
@@ -1,8 +0,0 @@
-// src/js/store/index.js
-
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers/index";
-
-const store = createStore(rootReducer);
-
-export default store;
