ac5f792439675966a6b6bd50f355a2773d13a885
hwagle <hwag3386@uni.sydney.edu.au>
2019-10-26 14:29:18 +1100

    fixing edit user

--- jobsite/src/src/main/java/com/neet/jobsite/bal/IUserService.java
+++ jobsite/src/src/main/java/com/neet/jobsite/bal/IUserService.java
@@ -18,9 +18,9 @@ public interface IUserService {
 	void AddUser(String firstName, String lastName, String email, String password, Integer userIntTypeValue,
 			List<String> skills, String education, String experience, String companyName);
 
-	boolean updateUser(String email, String education, String experience, String resume, List<String> skills);
-
 	void AddAdmin(String firstName, String lastName, String email, String password, Integer userIntTypeValue);
+
+	boolean updateUser(long userId, String education, String experience, String resume, List<String> skills);
 	
 	
 }
