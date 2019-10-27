83bf4ab999a4ea0b9f23a1760b7f1ec66ee12130
tchawla-usyd <52873601+tchawla-usyd@users.noreply.github.com>
2019-10-27 19:18:57 +1100

    Merge branch 'master' of https://bitbucket.org/tcha9078/ooaf-group-5 into Tushar_master
    # Conflicts:
    #	jobsite/src/target/m2e-wtp/web-resources/META-INF/MANIFEST.MF

--- jobsite/src/src/test/java/jobsite/UserAuthenticationControllerTest.java
+++ jobsite/src/src/test/java/jobsite/UserAuthenticationControllerTest.java
@@ -1,14 +1,62 @@
 package jobsite;
 
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import com.neet.jobsite.UserAuthenticationController;
+import com.neet.jobsite.bal.IUserService;
+import com.neet.jobsite.filter.CORSFilter;
+import com.neet.jobsite.model.User;
 
 import junit.framework.TestCase;
 
 public class UserAuthenticationControllerTest extends TestCase {
 	
+	@Mock
+	private UserAuthenticationController userController;
+	
+	@Mock
+	private IUserService userService;
+	
+	private MockMvc mockMvc;
+	
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .addFilters(new CORSFilter())
+                .build();
+    }
+	 
 	public void testHandlerRequest() throws Exception {
-		UserAuthenticationController controller = new UserAuthenticationController();
-		controller.authenticateByToken(null, null);
+		
+		User user = new User();
+		user.setFirstName("slha");
+		user.setLastName("slha");
+		user.setEmail("sf@gmail.com");
+		user.setPassword("sjafa");
+		user.setUserTypeID(4);
+		
+		
+		
+		HttpServletRequest request = mock(HttpServletRequest.class);
+	     HttpServletResponse response = mock(HttpServletResponse.class);
+	     
+	     mockMvc.perform(post("/authenticate/registerProcess"))
+         .andExpect(status().isOk())
+         .andReturn();
 		
 	}
 
