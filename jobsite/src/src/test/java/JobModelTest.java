99bf23e17ffb6dfa65cf2d5b1e8440d634392a32
tchawla-usyd <52873601+tchawla-usyd@users.noreply.github.com>
2019-10-27 22:31:09 +1100

    Committing Model Tests

---
+++ jobsite/src/src/test/java/JobModelTest.java
@@ -0,0 +1,36 @@
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.neet.jobsite.model.Job;
+
+
+public class JobModelTest {
+	
+	Job job = new Job(); // MyClass is tested
+
+	@Test
+	public void PropertySetTest() {
+		
+		Job job = new Job();
+		
+		job.setTitle("Developer");
+		job.setJobDescription("work as a developer");
+		job.setLocation("Sydney");
+		job.setJobCategoryID(1);
+		
+		
+		assertEquals(job.getTitle(),"Developer");
+		assertEquals(job.getJobDescription(),"work as a developer");
+		assertEquals(job.getLocation(),"Sydney" );
+		assertEquals(job.getJobCategoryID(), Integer.valueOf(1));
+		
+	}
+
+	// assert statements
+
+
+
+}
